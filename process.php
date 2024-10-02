38c7849dfce21425ab40606654cc5a06099d1ead
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-02 17:10:00 +0200

    collected form's data of index.php on process.php

--- process.php
+++ process.php
@@ -1,3 +1,8 @@
+<?php 
+$user_word = $_GET["userWord"];
+$user_text = $_GET["userText"];
+?>
+
 <!DOCTYPE html>
 <html lang="it">
     <head>
@@ -5,12 +10,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="author" content="Melvin Jerome Luna Maligaya">
         <title>php-badwords</title>
-        
-           <!-- Bootstrap -->
-           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+            <h1><?php echo $user_word; echo $user_text; ?></h1>
+          
+          
+            <!-- Bootstrap -->
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-           <!--Css-->
-           <link rel="stylesheet" href="css/style.css" type="text/css">
+            <!--Css-->
+            <link rel="stylesheet" href="css/style.css" type="text/css">
     </head>
     
     <body>
