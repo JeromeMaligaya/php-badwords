caa86f2f2f8186f19442d10521056c3609469a98
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-02 17:00:03 +0200

    added form on index.php

--- index.php
+++ index.php
@@ -14,7 +14,21 @@
     </head>
     
     <body>
-
+        <form id="form" class="mt-5 row justify-content-center" method="GET" action="./process.php">
+            <div class="mb-4 col-8">
+                <label for="userText" class="form-label">Type a text</label>
+                <textarea name="userText" class="form-control" id="userText" rows="3" placeholder="..."></textarea>
+            </div>
+            
+            <div class="mb-4 col-8">
+                <label for="userWord" class="form-label">Type the word to censure</label>
+                <input name="userWord" type="text" class="form-control" id="userWord" placeholder="...">
+            </div>
+            
+            <div class="col-8">
+                <button class="btn btn-primary" type="submit">Send</button>
+            </div>
+        </form>
         
         <!-- js bootstrap -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
