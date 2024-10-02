f792123141c59eaeb1be3bf0951e989960ee3539
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-02 17:19:55 +0200

    added strlen to user_text on process.php

--- process.php
+++ process.php
@@ -10,8 +10,6 @@ $user_text = $_GET["userText"];
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="author" content="Melvin Jerome Luna Maligaya">
         <title>php-badwords</title>
-            <h1><?php echo $user_word; echo $user_text; ?></h1>
-          
           
             <!-- Bootstrap -->
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -21,8 +19,12 @@ $user_text = $_GET["userText"];
     </head>
     
     <body>
+        <div class="m-4">
+            <h3>Length of your text: <span> <?php echo strlen($user_text); ?> </span></h3>
+            <p><?php echo $user_word . " " . $user_text; ?></p>
+        </div>
+       
 
-        
         <!-- js bootstrap -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
